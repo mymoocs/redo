409de36f9014ccac7c9b000b66819467e9ae2399
Arthur Vardanyan <artie.vard@gmail.com>
2015-11-08 21:11:25 +0400

    fix racing condition with haskell pragma, add target arg

---
+++ redo.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -fforce-recomp #-}
+import System.Environment (getArgs)
+import System.Process (createProcess, waitForProcess, shell)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  mapM_ redo args
+
+redo :: String -> IO ()
+redo target = do
+    (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
+    _ <- waitForProcess ph
+    return ()
