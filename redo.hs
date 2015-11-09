38dc62862442fc636c169fbf51ba5e4a65b45228
Arthur Vardanyan <artie.vard@gmail.com>
2015-11-09 20:30:56 +0400

    check process exitcodes

--- redo.hs
+++ redo.hs
@@ -1,5 +1,8 @@
 {-# OPTIONS_GHC -fforce-recomp #-}
+import System.Directory (renameFile, removeFile)
 import System.Environment (getArgs)
+import System.Exit (ExitCode(..))
+import System.IO (hPutStrLn, stderr)
 import System.Process (createProcess, waitForProcess, shell)
 
 main :: IO ()
@@ -9,6 +12,10 @@ main = do
 
 redo :: String -> IO ()
 redo target = do
-    (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do"
-    _ <- waitForProcess ph
-    return ()
+  let tmp = target ++"---redoing"
+  (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp 
+  exit  <- waitForProcess ph
+  case exit of
+    ExitSuccess -> do renameFile tmp target
+    ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
+                           removeFile tmp
