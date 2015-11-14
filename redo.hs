e1d6596f5cb2c57b7e337d76809019eba64c8132
Arthur Vardanyan <artie.vard@gmail.com>
2015-11-14 07:32:09 +0400

    ifchange concvept, trace for debug

--- redo.hs
+++ redo.hs
@@ -1,32 +1,50 @@
 {-# OPTIONS_GHC -fforce-recomp #-}
+{-# LANGUAGE StandaloneDeriving #-} 
 import Control.Monad (filterM, liftM)
+import Data.Map.Lazy (insert, fromList, toList, adjust)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (trace)
 import System.Directory (copyFile,renameFile, removeFile, doesFileExist)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
-import System.Process (createProcess, waitForProcess, shell)
+import System.Process (createProcess, waitForProcess, shell, CreateProcess(..), StdStream(..), CmdSpec(..))
+
+
+deriving instance Show CreateProcess
+deriving instance Show StdStream
+deriving instance Show CmdSpec
+
+trace' arg = trace (show arg) arg
+
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
   -- getArgs >>= mapM_ redo
   -- do getArgs >>= \args -> mapM_ redo args
 
+
 redo :: String -> IO ()
 redo target = do
   
   maybe printMissing redo' =<<redoPath target
   where
+    tmp :: FilePath
     tmp = target ++"---redoing"
+    printMissing :: IO ()
     printMissing = error "No .do file found for target"
     -- cmd path = "sh " ++ path  ++ " 0 " ++ takeBaseName target ++ " " ++ tmp  ++ " > " ++ tmp
-    cmd path = unwords ["sh", path, "0",takeBaseName target, tmp, ">",tmp]
+    cmd :: FilePath -> String
+    cmd path = trace' $ unwords ["sh", path, "0",takeBaseName target, tmp, ">",tmp] 
     redo' :: FilePath -> IO ()
     redo' path = do
-      (_, _, _, ph) <- createProcess $ shell $ cmd path
+      oldEnv <- getEnvironment
+      let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
+      --let newEnv = ("REDO_TARGET",target): (filter((/="REDO_TARGET") . fst) oldEnv)
+      (_, _, _, ph) <- createProcess $ trace' $ (shell $ cmd path) {env = Just newEnv}
       exit  <- waitForProcess ph
-      case exit of
+      case trace' exit of
         ExitSuccess -> do renameFile tmp target
         ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
                                removeFile tmp
