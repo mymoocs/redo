bc1f6cd556f529135f6d855bcfc7c033403b56a5
Arthur Vardanyan <artie.vard@gmail.com>
2015-11-15 16:16:29 +0400

    md5 check set up, exceptions

--- redo.hs
+++ redo.hs
@@ -1,48 +1,48 @@
 {-# OPTIONS_GHC -fforce-recomp #-}
-{-# LANGUAGE StandaloneDeriving #-} 
-import Control.Monad (filterM, liftM)
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+import Control.Exception (catch,IOException,  SomeException(..))
+import Control.Monad (filterM, liftM, unless)
 import Data.Map.Lazy (insert, fromList, toList, adjust)
 import Data.Maybe (listToMaybe)
-import Debug.Trace (trace)
-import System.Directory (copyFile,renameFile, removeFile, doesFileExist)
+import Data.Typeable (typeOf)
+import Debug.Trace(trace)
+import GHC.IO.Exception (IOErrorType(..)) 
+import System.Directory (copyFile,renameFile, removeFile, doesFileExist, getDirectoryContents)
 import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
-import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
+import System.FilePath (hasExtension, replaceBaseName, takeBaseName, (</>))
 import System.IO (hPutStrLn, stderr)
+import System.IO.Error (ioeGetErrorType)
 import System.Process (createProcess, waitForProcess, shell, CreateProcess(..), StdStream(..), CmdSpec(..))
 
-
-deriving instance Show CreateProcess
-deriving instance Show StdStream
-deriving instance Show CmdSpec
 
 trace' arg = trace (show arg) arg
 
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
-  -- getArgs >>= mapM_ redo
-  -- do getArgs >>= \args -> mapM_ redo args
 
 
 redo :: String -> IO ()
 redo target = do
-  
-  maybe printMissing redo' =<<redoPath target
+  upToDate' <- upToDate target
+  unless upToDate' $ maybe printMissing redo' =<< redoPath target
+  --if upToDate'
+  --  then return()
+  --  else  maybe printMissing redo' =<<redoPath target
   where
     tmp :: FilePath
     tmp = target ++"---redoing"
     printMissing :: IO ()
     printMissing = error "No .do file found for target"
-    -- cmd path = "sh " ++ path  ++ " 0 " ++ takeBaseName target ++ " " ++ tmp  ++ " > " ++ tmp
     cmd :: FilePath -> String
     cmd path = trace' $ unwords ["sh", path, "0",takeBaseName target, tmp, ">",tmp] 
     redo' :: FilePath -> IO ()
     redo' path = do
       oldEnv <- getEnvironment
       let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
-      --let newEnv = ("REDO_TARGET",target): (filter((/="REDO_TARGET") . fst) oldEnv)
-      (_, _, _, ph) <- createProcess $ trace' $ (shell $ cmd path) {env = Just newEnv}
+      (_, _, _, ph) <- createProcess $ (shell $ cmd path) {env = Just newEnv}
       exit  <- waitForProcess ph
       case trace' exit of
         ExitSuccess -> do renameFile tmp target
@@ -52,10 +52,32 @@ redo target = do
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target =  listToMaybe  `liftM` filterM doesFileExist candidates
-  -- liftM safeHead $ filterM doesFileExist candidates
-  -- existingCandidates <- filterM doesFileExist candidates
-  -- return $ safeHead existingCandidates 
   where
       candidates = [target++".do"] ++ if hasExtension target
                                       then [replaceBaseName target "default" ++ ".do"]
                                       else  []
+
+upToDate :: FilePath -> IO Bool
+upToDate target = catch
+  (do deps <- getDirectoryContents depDir
+      (trace' . all id) `liftM` mapM depUpToDate deps)
+  (\(e :: IOException) -> return False)
+  where depDir = ".redo/" </> target
+        depUpToDate :: FilePath -> IO Bool
+        depUpToDate  dep = catch
+          (do oldMD5 <- trace' `liftM`  readFile (depDir </> dep)
+              return False)
+          (\e -> return (ioeGetErrorType e == InappropriateType))
+
+--3.
+--          (\e -> do if ioeGetErrorType e == InappropriateType
+--                       then return True
+--                       else return False)
+        
+--2.
+--          (\e -> do hPutStrLn stderr $ show $ ioeGetErrorType e
+--                    return False)
+
+--1.
+--          (\(SomeException e) -> do hPutStrLn stderr $ show $ typeOf e
+--                                  return False)
